6daf5786c1ccb1481aab049f22613aa145ef2b72
drhongos <lucasziegenfuhs@gmail.com>
2023-08-30 20:41:05 +0200

    added chain-status call

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ccip::{
     get_router,
     get_selector,
     get_lane,
+    get_status_on_chain,
 };
 
 #[derive(Debug, Parser)]
@@ -18,6 +19,10 @@ enum Command {
     GetRouter {chain_name: String},
     GetSelector {chain_name: String},
 //    GetFeeTokens {chain_name: String, selector: u8},
+    ChainStatus {
+        #[arg(short, long)]
+        chain_name: String
+    },
     GetLane {
         #[arg(short, long)]
         origin: String,
@@ -30,7 +35,7 @@ enum Command {
 async fn main() {
     let args = Cli::parse();
     println!("Chainlink-rs CLI");
-//    dotenv::dotenv().ok();
+    dotenv::dotenv().ok();
 //    let rpc_url = dotenv::var("RPC_URL").expect("No RPC_URL found in .env");
 
     match &args.command {
@@ -52,7 +57,13 @@ async fn main() {
             let chain_d = get_chain(&destination).expect("Error with destination");
             let lane = get_lane(chain_s, chain_d).expect("Error looking for lane");
             println!("{:#?}", lane);
-        }
+        },
+        Some(Command::ChainStatus { chain_name }) => {
+            let chain = get_chain(chain_name).expect("Error with chain selected");
+            let pk = dotenv::var("PRIVATE_KEY").expect("No private key supplied to .env");         
+            get_status_on_chain(pk, chain).await.expect("Error getting status");
+            //println!("{:#?}", user_status);
+        },
         _ => println!("Command unknown"),
     }
 }
