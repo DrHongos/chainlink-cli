69a0700b8dff190c879716b8a1b90704d6b78000
drhongos <lucasziegenfuhs@gmail.com>
2024-02-09 21:33:47 +0100

    removed dependencies unused

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use ccip::{
     get_selector,
     get_lane,
 };
-use alloy_providers::provider::{Provider, TempProvider};
+use alloy_providers::provider::Provider;
 use alloy_transport_http::Http;
 use reqwest::Client;
 use alloy_rpc_client::ClientBuilder;
@@ -17,7 +17,6 @@ use alloy_chains::Chain;
 use eyre::Result;
 use std::{str::FromStr, sync::Arc};
 use datafeeds::OraclesIndex;
-use alloy_sol_types::{SolType, SolCall};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -38,7 +37,6 @@ pub struct PairSetting {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    Test {},
     // Data feeds
     GetOracle {
         #[clap(flatten)]
@@ -108,67 +106,6 @@ async fn main() {
     dotenv::dotenv().ok();
     let rpc_url_id = std::env::var("RPC_URL_ID").expect("No RPC_URL_ID in .env");
     match &args.command {
-        Some(Command::Test {}) => {
-            println!("Call aggregator with lastRoundData");
-            let chain = Chain::from_named(alloy_chains::NamedChain::Mainnet);
-            let provider = get_provider(chain, &rpc_url_id).unwrap();
-            
-            let aggregator = "0x37bC7498f4FF12C19678ee8fE19d713b87F6a9e6".parse::<alloy_primitives::Address>().unwrap();
-            let aggregator_2 = "0xd3fCD40153E56110e6EEae13E12530e26C9Cb4fd".parse::<alloy_primitives::Address>().unwrap();
-            // this works
-            
-            println!("Calling {} individually", aggregator);
-            match functions::datafeeds::get_latest_round_data(provider.clone(), aggregator).await {
-                Ok(r) => println!("{:?}", r),
-                Err(e) => println!("{:?}", e)
-            }
-            println!("Calling {} individually", aggregator_2);
-            match functions::datafeeds::get_latest_round_data(provider.clone(), aggregator_2).await {
-                Ok(r) => println!("{:?}", r),
-                Err(e) => println!("{:?}", e)
-            }
-           
-            println!("Calling both with multicall3");
-            // now replicate with a multicall
-            let mc = functions::multicall3::aggregate3Call {
-                calls: vec![
-                    functions::multicall3::Call3 {
-                        target: aggregator,
-                        allowFailure: true,
-                        callData: datafeeds::contracts::EACAggregatorProxy::EACAggregatorProxy::latestRoundDataCall{}.abi_encode().into(),
-                    },
-                    functions::multicall3::Call3 {
-                        target: aggregator_2,
-                        allowFailure: true,
-                        callData: datafeeds::contracts::EACAggregatorProxy::EACAggregatorProxy::latestRoundDataCall{}.abi_encode().into(),
-                    }
-                ]
-            };
-            let data = alloy_rpc_types::CallInput::new(mc.abi_encode().into());                    
-            let tx = alloy_rpc_types::CallRequest {
-                to: Some(constants::MULTICALL3.parse::<alloy_primitives::Address>().unwrap()),
-                input: data,
-                ..Default::default()
-            };
-            match provider.call(tx, None).await {
-                Ok(res) => {
-                    let responses = functions::multicall3::MultiResult::abi_decode(&res, false).unwrap();      
-                    println!("Got {} responses", responses.len());
-                    for response in responses.into_iter() {
-                        if response.success {
-                            if let Ok(r) = <functions::datafeeds::GetRoundDataReturn as alloy_sol_types::SolValue>::abi_decode(&response.returnData, false) {
-                                println!("{:?}", r)
-                            } else {
-                                println!("Error on result for")
-                            }
-                        } else {
-                            println!("Response w error")
-                        }
-                    }
-                },
-                Err(e) => panic!("Error in call {:?}", e)
-            }
-        },
         
         // Data Feeds
         Some(Command::GetOracle { args }) => {
@@ -254,84 +191,6 @@ async fn main() {
                             }
                         },
                     }
-                }
-            }
-        },
-        Some(Command::GetAllPhases { args }) => {
-            let chain = Chain::from_str(&args.chain).expect("chain not found");
-            if let Ok(provider) = get_provider(chain, &rpc_url_id) {
-                let price_feeds = OraclesIndex::load_reference_feeds(chain).await;
-                if let Some(oracle) = price_feeds.get_oracle(&args.base.to_uppercase(), &args.quote.to_uppercase()) {
-                    let all_phases = functions::datafeeds::get_aggregators(provider.clone(), oracle).await;
-                    
-                    let versions = functions::datafeeds::get_aggregators_version(provider.clone(), all_phases.clone()).await.expect("Couldn't get versions");
-                    
-                    // versions 2 and below require extra fetching
-                    let mut aggs_above_2 = Vec::new();                    
-                    
-                    // doing this because cannot do multicall on lastRoundData... FIX ME
-                    let mut last_round_data = std::collections::HashMap::<alloy_primitives::Address, functions::datafeeds::GetRoundDataReturn>::new();
-                    for (a, v) in all_phases.into_iter().zip(versions.clone()) {
-                        if v.gt(&alloy_primitives::U256::from(2)) {
-                            // THIS WORKS 
-                            if let Ok(latest_round_data) = functions::datafeeds::get_latest_round_data(provider.clone(), a.clone()).await {
-                                last_round_data.insert(a.clone(), latest_round_data);
-                                aggs_above_2.push(a);
-                            }
-                            //println!("{}:\n{:?}", a, latest_round);
-                        }
-                    }
-                    
-                    /*
-                        methods failing:
-                        getTimestamp 
-                        latestTimestamp
-                        latestRoundData
-                        
-                        - multicalls results are all success: false.. 
-                        - even for AggregatorContract & AccessControlledAggregator interfaces
-                        - using etherscan, some values are ok (and cannot retrieve it here)
-                    */
-                    // this method fails.. still idk why
-                    //let time_starts = functions::datafeeds::find_addresses_last_round_data(provider.clone(), aggs_above_2.clone()).await;
-                    //println!("{:?}", time_starts);
-/*                     
-                    for agg in aggs_above_2.clone().into_iter() {
-                        println!("Aggregator {} last block data\n{:?}", agg, last_round_data.get(&agg).unwrap());
-                    }
- */                    
-                    // testing
-
-                    // perform a search of historical data in each aggregator, using get_multiple_round_data()
-                    // check the failing cases (hipothesis: only roundIds in the aggregator..)
-
-                    let last_round_proxy_round_id = functions::datafeeds::get_latest_round_data(provider.clone(), oracle.proxy_address.unwrap()).await.unwrap().roundId;                    
-                    // lets get some roundData's
-                    let current_aggregator_address = aggs_above_2.last().unwrap(); 
-                    let current_aggregator_last_round_id = last_round_data.get(current_aggregator_address).unwrap().roundId;
-                    let from_last_aggregator = last_round_proxy_round_id - current_aggregator_last_round_id;
-                    println!("Total roundId is {} and {} are from the current aggregator", last_round_proxy_round_id, current_aggregator_last_round_id);
-                    println!("Create a vector from {} to {}", from_last_aggregator, last_round_proxy_round_id);
-                    
-                    // take a few
-                    //let round_id_acum: Vec<u128> = (from_last_aggregator+10..last_round_proxy_round_id).collect();       
-                    //let shorted_list_round_ids = round_id_acum.into_iter().take(10).collect();
-                    
-                    // last ten round ids
-                    let shorted_list_round_ids: Vec<u128> = (last_round_proxy_round_id-10..last_round_proxy_round_id).collect();       
-                    println!("Aggregator address {}", current_aggregator_address);
-                    let res = functions::datafeeds::get_multiple_round_data(
-                        provider, 
-                        oracle.proxy_address.unwrap(),
-                    //    current_aggregator_address.clone(),   // should be this one.. but it fails 
-                        shorted_list_round_ids
-                    ).await;
-                    if let Ok(res) = res {
-                        for r in res {
-                            println!("{:?}", r);
-                        }                    
-                    }    
-                    // it fails! seems like round_id's aren't trivial... AGAIN
                 }
             }
         },
